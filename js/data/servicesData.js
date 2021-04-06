74cc34c5ef45c3dc92f6ae2fab64f9d566383db1
AudriusKru <krutulis.audrius@gmail.cm>
2021-04-06 20:43:45 +0300

    save

---
+++ js/data/servicesData.js
@@ -0,0 +1,14 @@
+const servicesData = [
+    {
+        img: '.img',
+        title: 'it design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+        link: '#',
+        active: true
+    }
+
+];
+
+const servicesData = 
+
+export { servicesData }
