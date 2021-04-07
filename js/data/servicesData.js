3cb50caaa72423d4f68a479deebc74f27ca894a9
AudriusKru <krutulis.audrius@gmail.cm>
2021-04-07 09:08:19 +0300

    max

--- js/data/servicesData.js
+++ js/data/servicesData.js
@@ -1,34 +1,36 @@
-const servicesData = [
-    {
-        img: '.img',
-        title: 'it design',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
-        link: '#',
-        active: true
-    },
-    {
-        img: '.img',
-        title: 'it design',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
-        link: '#',
-        active: false
-    },
-    {
-        img: '.img',
-        title: 'it design',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
-        link: '#',
-        active: true
-    },
-    {
-        img: '.img',
-        title: 'it design',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
-        link: '#',
-        active: true
-    },
-];
-
-const servicesData = 
+const servicesData = {
+    imgPath:   '.img/services/',
+    maxCount: 3,
+    list: [
+        {
+            img: 'mitech-box-image-style-01-image-01-100x108.png',
+            title: 'it design',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+            link: '#',
+            active: true
+        },
+        {
+            img: 'mitech-box-image-style-01-image-01-100x108.png',
+            title: 'it design',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+            link: '#',
+            active: false
+        },
+        {
+            img: 'mitech-box-image-style-01-image-01-100x108.png',
+            title: 'it design',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+            link: '#',
+            active: true
+        },
+        {
+            img: 'mitech-box-image-style-01-image-01-100x108.png',
+            title: 'it design',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+            link: '#',
+            active: true
+        },
+    ]
+}
 
 export { servicesData }
