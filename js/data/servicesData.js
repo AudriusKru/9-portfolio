7b410d568d33fa31c9f2a4f8d638f6b32029a665
AudriusKru <krutulis.audrius@gmail.cm>
2021-04-07 09:02:45 +0300

    chame services data

--- js/data/servicesData.js
+++ js/data/servicesData.js
@@ -5,8 +5,28 @@ const servicesData = [
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
         link: '#',
         active: true
-    }
-
+    },
+    {
+        img: '.img',
+        title: 'it design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+        link: '#',
+        active: false
+    },
+    {
+        img: '.img',
+        title: 'it design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+        link: '#',
+        active: true
+    },
+    {
+        img: '.img',
+        title: 'it design',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt velit accusamus voluptas',
+        link: '#',
+        active: true
+    },
 ];
 
 const servicesData = 
